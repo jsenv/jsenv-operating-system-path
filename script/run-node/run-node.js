f1442b4b69549401479efdded58172a18c3f4c81
Damien Maillard <damien.maillard@dailymotion.com>
2019-08-23 14:12:07 +0200

    update dev stuff

--- script/run-node/run-node.js
+++ script/run-node/run-node.js
@@ -1,12 +1,9 @@
-const { execute, launchNode } = require("@jsenv/core")
+const { execute } = require("@jsenv/execution")
+const { launchNode } = require("@jsenv/node-launcher")
 const { projectPath } = require("../../jsenv.config.js")
-const { getFromProcessArguments } = require("./getFromProcessArguments.js")
-
-const filenameRelative = getFromProcessArguments("file")
 
 execute({
   projectPath,
   launch: launchNode,
-  fileRelativePath: `/${filenameRelative}`,
-  mirrorConsole: true,
+  fileRelativePath: `/${process.argv[2]}`,
 })
